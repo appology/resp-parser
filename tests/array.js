b2e442d47e9de621d674087700ea95b071a3dd00
Ryan Haney <ryan@appology.net>
2017-09-18 01:54:40 -0700

    Test for nested arrays now passing

--- tests/array.js
+++ tests/array.js
@@ -33,3 +33,11 @@ test('multi-typed-array-split-feedings', t => {
   var expected = [{"type":"Array","length":2,"value":[{"type":"Integer","value":0},{"type":"BulkString","length":5,"value":"Item1"}]}]
   t.deepEqual(value, expected, 'Parsed value does not match expected result')
 })
+
+test('nested-array', t => {
+  var parser = new RespParser()
+  parser.feed('*2\r\n*2\r\n:0\r\n:1\r\n*2\r\n:2\r\n:3\r\n')
+  var value = parser.results
+  var expected = [{"type":"Array","length":2,"value":[{"type":"Array","length":2,"value":[{"type":"Integer","value":0},{"type":"Integer","value":1}]},{"type":"Array","length":2,"value":[{"type":"Integer","value":2},{"type":"Integer","value":3}]}]}]
+  t.deepEqual(value, expected, 'Parsed value does not match expected result')
+})
