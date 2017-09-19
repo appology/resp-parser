369918921eec7f4621b5ef7d1b918158d4154c64
Ryan Haney <ryan@appology.net>
2017-09-18 22:06:07 -0700

    Correct output of shell errors for CI detection

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
 var shell = require('shelljs')
-shell.exec('ava test/**/*.js --color=always')
+
+if(process.env.CI !== 'true' && shell.exec('npm run build').code != 0){
+  shell.echo('Error: Project failed to build.');
+  shell.exit(1);
+}
+
+if(shell.exec('ava test/**/*.js --color=always').code != 0){
+  shell.echo('Error: One or more tests failed.');
+  shell.exit(1);
+}
