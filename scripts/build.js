1e983f92d7ae3b42c4761f106470d2525b7a7412
Ryan Haney <ryan@appology.net>
2017-09-19 18:21:04 -0700

    Separated scripts for nearley, babel, and build

--- scripts/build.js
+++ scripts/build.js
@@ -1,9 +1,9 @@
 var shell = require('shelljs')
 
-if(shell.exec('node node_modules/nearley/bin/nearleyc.js src/nearley/grammar.ne > src/nearley/grammar.js --color=always').code != 0) {
+if(shell.exec('npm run nearley').code != 0) {
   shell.exit(1);
 }
 
-if(shell.exec('babel src -d lib --color=always').code != 0) {
+if(shell.exec('npm run babel').code != 0) {
   shell.exit(1);
 }
