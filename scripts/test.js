b693af1751ede460f4dce04b3f874eca9fa8cfea
Ryan Haney <ryan@appology.net>
2017-09-19 18:17:58 -0700

    Removed build from test, as we are testing from src/

--- scripts/test.js
+++ scripts/test.js
@@ -1,9 +1,5 @@
 var shell = require('shelljs')
 
-if(process.env.CI !== 'true' && shell.exec('npm run build --color=always').code != 0){
-  shell.exit(1);
-}
-
 if(shell.exec('ava test/**/*.js --color=always').code != 0){
   shell.exit(1);
 }
